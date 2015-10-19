60e41673e5968f0e35567a7b8fe91ca3236546cc
brenteritou <brent.eritou@boldradius.com>
2015-10-19 13:06:58 -0400

    Move basic requester to its own file

--- src/test/scala/github/basic/GitHubSearchBasicScenario.scala
+++ src/test/scala/github/basic/GitHubSearchBasicScenario.scala
@@ -2,7 +2,6 @@ package github.basic
 
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
-import io.gatling.http.Predef._
 
 object GitHubSearchBasicScenario {
   val search: ScenarioBuilder = scenario("Search for Gatling on GitHub")
@@ -11,10 +10,4 @@ object GitHubSearchBasicScenario {
     .exec(GitHubSearchBasicRequester.searchForGatling)
     .pause(1)
     .exec(GitHubSearchBasicRequester.getGatlingPage)
-}
-
-object GitHubSearchBasicRequester {
-  val getHomepage = http("github-home").get("/")
-  val searchForGatling = http("search-gatling").get("/search?utf8=✓&q=gatling")
-  val getGatlingPage = http("gatling-on-github").get("/gatling/gatling")
 }
